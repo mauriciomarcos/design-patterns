ca44c11d6b83d3fffb02d186eb660c3e61d7f452
Maurício Marcos <mauricio.marcos@beblue.com.br>
2022-04-22 08:32:14 -0300

    Implementação Pattern Singleton.

---
+++ DesignParttensSolution/src/Singleton/ImplementationSingleton/DocumentSingleton.cs
@@ -0,0 +1,23 @@
+﻿namespace Singleton.ImplementationSingleton
+{
+    internal class DocumentSingleton
+    {
+        private DocumentSingleton()
+        { }
+
+        private static DocumentSingleton instance;
+
+        public static DocumentSingleton Instance
+        {
+            get 
+            { 
+                if (instance is null)
+                {
+                    instance = new DocumentSingleton();
+                }
+
+                return instance;
+            }
+        }
+    }
+}
---
+++ DesignParttensSolution/src/Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using Singleton.ImplementationSingleton;
+using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main()
+        {
+            var document = DocumentSingleton.Instance;
+            var documento2 = DocumentSingleton.Instance;
+
+            if (document.Equals(documento2))
+                Console.WriteLine("Pattern Singleton executado com sucesso");
+        }
+    }
+}
